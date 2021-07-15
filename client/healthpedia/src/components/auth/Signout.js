62c82f86ef9e5ecdd5cb8d1e33435a869b6b075a
Sr-GrEeNe <81869226+Sr-GrEeNe18@users.noreply.github.com>
2021-07-15 12:59:09 -0400

    added react folder structure

---
+++ client/healthpedia/src/components/auth/Signout.js
@@ -0,0 +1,30 @@
+
+import React from "react";
+import {useDispatch} from 'react-redux';
+import {signOut} from '../../actions/index';
+import {useHistory} from 'react-router-dom'
+
+const Signout = () => {
+
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const logout = (e) => {
+    
+    dispatch(signOut(()=>{
+      console.log('user logging out');
+        history.push('/')
+    }))
+  }
+
+  return <div style={{height: "100vh"}} className="d-flex flex-column justify-content-center align-items-center">
+  <h1 className="text-white">Goodbye</h1>
+
+  <h3 className="text-warning">...sorry to see you go!</h3>
+  <h5 className="text-info">This page should log the user out of the application by clearing the local storage and the redux store.</h5>
+
+  <button onClick={logout}>Sign Out</button>
+ </div>
+};
+
+export default Signout;
---
+++ client/healthpedia/src/components/auth/Signup.js
@@ -0,0 +1,68 @@
+
+import React, {useState} from "react";
+import {useDispatch} from 'react-redux';
+import { Link } from 'react-router-dom';
+import {signUp} from '../../actions/index';
+import {useHistory} from 'react-router-dom';
+
+
+const Signup = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  const handleSubmit = (e) => {
+    
+    e.preventDefault();
+
+    //collect information from form
+    //dispatch signUp action
+    //dispatch(signUp({}, cb))
+    dispatch(signUp({
+      email: email,
+      password: password
+    }, ()=>{
+      history.push('/feature')
+    }))
+  }
+
+  return (
+  <div className="mt-5">
+  
+    <div className="grid align__item">
+
+      <div className="register">
+
+            <img height="100px" src="https://avatars.githubusercontent.com/u/67744643?s=200&v=4" alt=""/>
+
+            <h2>Sign Up</h2>
+
+            <form onSubmit={handleSubmit} className="form">
+
+              <div className="form__field">
+                
+                <input type="email" value={email} onChange={e=>setEmail(e.target.value)} placeholder="enter email address" />
+              </div>
+
+              <div className="form__field">
+                <input type="password" value={password} onChange={e=>setPassword(e.target.value)} placeholder="enter password" />
+              </div>
+
+              <div className="form__field">
+                <input type="submit" value="Sign Up" />
+              </div>
+
+          </form>
+
+          <p>Already have an account? <Link to="/signin">Log in</Link></p>
+
+      </div>
+
+    </div>
+  
+  </div>);
+};
+
+export default Signup;
