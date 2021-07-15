933273d9f4f4e3d9397de2167ed795b3d2b3985e
analiticofx <79728365+analiticofx@users.noreply.github.com>
2021-07-15 14:54:37 -0400

    updated all react folders to make a basic page with a login, log out, and register form

--- client/healthpedia/src/components/layout/Header.js
+++ client/healthpedia/src/components/layout/Header.js
@@ -10,7 +10,7 @@ const Header = () => {
   return (
     <>
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-    <Link className="navbar-brand" to="#"><img src={dcLogo} height="50px" /> - Client Side Authentication with JWTs</Link>
+    <Link className="navbar-brand" to="#"><img src={dcLogo} height="50px" /></Link>
     <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
     </button>
@@ -20,10 +20,10 @@ const Header = () => {
                 <Link className="nav-link" to="/">Home <span className="sr-only">(current)</span></Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/welcome">Welcome <span className="sr-only">(current)</span></Link>
+                <Link className="nav-link" to="/welcome">Welcome <span className="sr-only">(unprotected)</span></Link>
             </li>
             <li className="nav-item">
-                <Link className="nav-link" to="/feature">Feature</Link>
+                <Link className="nav-link" to="/feature">Healthpedia</Link>
             </li>
             <li className="nav-item">
                 <Link className="nav-link" to="/signup">Register</Link>
--- client/healthpedia/src/index.js
+++ client/healthpedia/src/index.js
@@ -2,30 +2,63 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Welcome from './components/Welcome';
-import Feature from './components/Feature'; //protect
+import Feature from './components/ProtectedContent'; //protect
 import Signin from './components/auth/Signin';
 import Signout from './components/auth/Signout';
 import Signup from './components/auth/Signup';
 import BaseLayout from './components/layout/BaseLayout';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import  './assets/styles.scss';
+// import  './assets/styles.scss';
 import {createStore, applyMiddleware, compose} from 'redux';
 import {Provider} from 'react-redux';
 import reduxThunk from 'redux-thunk'
-import reducer from './reducers/reducer';
+import reducer from './reducers/healthReducers';
 import requireAuth from './requireAuth';
 import Auth from './RequireAuthHooks';
 import {
   BrowserRouter as Router,
   Route, Switch
 } from 'react-router-dom'
-import './index.css';
-import App from './App';
+// import './index.css';
 
+
+// initializing redux store
+// requires a reducer. Second argument is for redux dev-tools extension.
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  reducer,
+  {},
+  composeEnhancers(applyMiddleware(reduxThunk))
+);
+
+
+//provider hooks react to redux.  
+//Must pass redux instance to provider via "store" prop.
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+    <Router>
+      <BaseLayout>
+          <Switch>
+            <Route exact path='/' component={App}/>
+            <Route path='/welcome' component={Welcome}/>
+            <Route path='/signup' component={Signup}/>
+            <Route exact path="/feature" render={()=>{
+              <Auth>
+                <Feature />
+              </Auth>
+            }} />
+
+            {/* <Route path='/feature' component={requireAuth(Feature)}/> */}
+            <Route path='/signout' component={Signout}/>
+            <Route path='/signin' component={Signin}/>
+          </Switch>
+      </BaseLayout>
+      </Router>
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
