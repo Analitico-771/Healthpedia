39f478323cd0bddd8392c3344f3bc0f441e36c5a
analiticofx <79728365+analiticofx@users.noreply.github.com>
2021-07-21 12:37:29 -0400

    added new componenet for displaying information and added new logo

--- client/healthpedia/src/components/HealthArticles.js
+++ client/healthpedia/src/components/HealthArticles.js
@@ -3,6 +3,7 @@ import React, {useState, useEffect} from "react";
 import { useDispatch, useSelector } from 'react-redux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { addSubjectData } from '../actions/index'
+import RelatedItem from "./RelatedItem";
 import './healthpedia.css'
 
 
@@ -11,7 +12,8 @@ const HealthInfo = () => {
 
     const subjectData =  useSelector(state => state.subjectInfo);
     // console.log(subjectData);
-    console.log("Resource", subjectData.Result.Resources.Resource);
+    console.log("Resource", subjectData);
+    //console.log("Resource", subjectData.Result.Resources.Resource);
     // console.log(subjectData.Result.Resources.Resource.RelatedItems);
     // const [healthData, setHealthData] = useState([]);
     // setHealthData(subjectInfo.RelatedItems.RelatedItem);
@@ -20,7 +22,7 @@ const HealthInfo = () => {
     //     array: subjectInfo.Result.Resources.Resource.RelatedItems.RelatedItem
     // }
     // console.log(healthData);
-    // console.loghealthData.Result.Resources.R(esource.RelatedItems.RelatedItem);
+    // console.log(healthData.Result.Resources.Resource.RelatedItems.RelatedItem);
     // console.log(subjectInfo.Result.Resources.Resource.RelatedItems.RelatedItem);
     // const healthData = [
     //     data: subjectInfo
@@ -37,35 +39,31 @@ const HealthInfo = () => {
         //     effect
         //   };
     })//componenentDidMount
-
+    // console.log(subjectData);
+    // console.log(subjectData.Result.Resources.Resource)
+    // console.log(subjectData.Result.Resources.Resource[0].RelatedItems.RelatedItem)
 
     return <>
         
         <div style={{height: "100vh"}} className="d-flex flex-column align-items-center">
-        <h1 className="text-white">HealthInfo Page</h1>
-        <h3 className="text-warning">This is place holder text{'\u00A0'} {'\u00A0'}</h3>
-        <h5 className="text-info">This is place holder text</h5>
+        <h1 className="text-white">Welcome to Helthpedia Information </h1>
 
-        {/* begin mapping health Data */}
-        <div className="health-table d-flex flex-column"> {subjectData.Result.Resources.Resource.map(subjectDataObj => {
-            return <>
-                <div className="health-table d-flex flex-column"> {subjectDataObj.RelatedItems.RelatedItem.map(relatedItemDataObj => {
-                return <>
-                    <div className="align-middle p-2">{`${relatedItemDataObj.Title}\u00A0\u00A0`} 
-                    
-                        <button className="favorites" id={`${relatedItemDataObj.Id}`}>
-                            Favorite
-                        </button>
-                    </div>
-                </>;
-                })}
-                {/* end mapping health Data */}
-                </div> 
-                </>;
-             })}
-            {/* end mapping health Data */}
-            </div>
+        <h5 className="text-info align-items-center">You can read more about the Topics or Categories you selected. You can also 'Click' on a link to read more about a specific subject of interest</h5>
+
+        {/* <div className="text-info">Topic ID: {`${subjectData.Result.Query.TopicId}`}</div> */}
+
+        {/* <img src={`${subjectData.Result.Resources.["ImageUrl"].Resource}`} width="500" height="600"></img> */}
+        
+        
+        {subjectData.Result.Resources.Resource[0].Sections.section.map((section, index) => {
+            return<>
+
+                <RelatedItem id={index} title={section.Title} content={section.Content}/>
+                         {/* index, title, and content are props */}
+            </>
+        })}
         </div>
+
     </>
 }
 
