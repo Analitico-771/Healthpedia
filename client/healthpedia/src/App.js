a489dd032f157e14671e9aac96709cd3fc07a1a0
analiticofx <79728365+analiticofx@users.noreply.github.com>
2021-07-22 16:33:27 -0400

    added favorite star and redux logic

--- client/healthpedia/src/App.js
+++ client/healthpedia/src/App.js
@@ -1,11 +1,15 @@
 
 import React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'//testing font awesome
+
+
 
 const App = (props) => {
+
   return (
     
     <div className="d-flex flex-column justify-content-center align-items-center">
+
+      <br/>
       
       <h1 className="text-white">
        Welcome to Healthpedia
@@ -13,23 +17,15 @@ const App = (props) => {
 
       <br/>
 
-      <h3 className="text-warning">
-
-       Testing Font Awesome
-
-       {'\u00A0'} {'\u00A0'}
-
-       <button className="favorites" >Favorite</button>
-       
-       {'\u00A0'} {'\u00A0'} 
-       
-       <FontAwesomeIcon className="favorites" icon="faStar" />
-
+      <h3 className="text-warning"> Place Holder for content yellow{'\u00A0'} 
+      
+      
+      
       </h3>
 
       <h5 className="text-info">
         
-        This is a place holder text for content
+        This is a place holder text for content blue
         
       </h5>
       
