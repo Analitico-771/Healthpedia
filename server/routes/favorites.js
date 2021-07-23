49f36b0b40bca4d9ec627344de7b8469919c0221
analiticofx <79728365+analiticofx@users.noreply.github.com>
2021-07-23 12:59:23 -0400

    working on favorites route

--- server/routes/favorites.js
+++ server/routes/favorites.js
@@ -1,17 +1,28 @@
+
 const express = require('express');
 const router = express.Router();
 router.use(express.json())
+
 const passport = require('passport');
 const db = require('../models');
+
 let requireJwt = passport.authenticate('jwt', {session: false})
 //to be changed to add favorites
-router.post('/favorites', requireJwt, async(req, res) => {
+
+router.post('/favorites', async(req, res) => {
     let apiId = req.body.apiId;
     let types = req.body.types;
     let title = req.body.title;
+
+    console.log("favorite route running")
+
     try{
-        await db.favorites.create({apiId: apiId, types: types, title: title})
-        return res.send("success")
+        let favorites = await db.favorites.create({apiId: apiId, types: types, title: title})
+        // let jwtToken = token(favorites);
+        // return res.json({token: jwtToken})
+
+        res.send("success");
+        return res.json(favorites)
     }
     catch(error){
         return res.send(error)
