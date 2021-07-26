1ec640500dceeabad6fbf20ca7433fe2ae74acf4
analiticofx <79728365+analiticofx@users.noreply.github.com>
2021-07-26 12:08:31 -0400

    heroku pre-build

--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
-const PORT = 3001
 const dotenv = require('dotenv').config();
+const PORT = process.env.PORT || 3001
 
 const apiKey = process.env.REACT_APP_API_KEY;
 const secrets = process.env.REACT_APP_SECRETS;
@@ -13,27 +13,3 @@ app.use(require('./routes/authentication'));
 app.listen(PORT, ()=>{
     console.log(`listening on port ${PORT}`);
 })
-
-
-// const jwt = require('jwt-simple');
-
-
-
-// let userInfo = {
-//     id: '12345',
-//     userName : 'Veronica', 
-//     email: 'me@me.com'
-// }
-
-// const token = (user) => {
-    
-//     let timestamp = new Date().getTime();
-
-//     return jwt.encode({sub: user.id, name: user.userName, iat: timestamp}, "lasdkj;flsajl;dfjal;sdjfl;askjdfl;ksajdl;kfs")
-// }
-
-// let tokenID = token(userInfo);
-
-// console.log(tokenID);
-
-// console.log(jwt.decode(tokenID, "lasdkj;flsajl;dfjal;sdjfl;askjdfl;ksajdl;kfs"));
